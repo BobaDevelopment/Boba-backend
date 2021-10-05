e4942bc5bd7255fb2753ff14b23321e9f8f5e2fd
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-10-05 22:54:17 +0800

    feat(end): Generate invite code and set in redis database

--- app/user/room.py
+++ app/user/room.py
@@ -3,6 +3,7 @@ from flask import request, jsonify
 from app.model.dbmodel import *
 from app.utils.jwt import getToken
 from app.model.response import *
+from app import r
 # joinroom and leaveroom
 # emit user to drop
 
@@ -25,7 +26,13 @@ def userCreateRoom():
         prize = Prize(roomId=room.id, name=name, picture=picurl, level=level, count=count)
         db.session.add(prize)
     db.session.commit()
-    return jsonify(OK())
+    from datetime import datetime
+    t = datetime.now()
+    inviteCode = str(int("%02d%02d%02d" % (t.hour+5, t.minute+4, t.second+3)))
+    r.set(inviteCode, str(room.id))
+    return jsonify(OK(
+        invitecode = inviteCode
+    ))
 
 
 # 删除房间
