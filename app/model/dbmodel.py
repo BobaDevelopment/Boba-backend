a3be42f51c6de71a66b663fad0bb066f9b6cbe63
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-10-08 17:02:46 +0800

    feat(end): add username and user avatar column

--- app/model/dbmodel.py
+++ app/model/dbmodel.py
@@ -8,6 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     OpenID = db.Column(db.String(255), unique=True)
     sessionKey = db.Column(db.String(255))
+    username = db.Column(db.String(255))
+    avatar = db.Column(db.String(255))
 
 
 class Room(db.Model):
